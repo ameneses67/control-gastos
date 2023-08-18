970be34ed0df8d3d28a401892a5c94419f735916
Poncho <ameneses67@gmail.com>
2023-08-17 22:45:14 -0600

    primer commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import { useState } from "react";
+import Header from "./components/Header";
+
+function App() {
+  const [presupuesto, setPresupuesto] = useState(0);
+
+  return <Header presupuesto={presupuesto} setPresupuesto={setPresupuesto} />;
+}
+
+export default App;
---
+++ src/components/NuevoPresupuesto.jsx
@@ -0,0 +1,39 @@
+import { useState } from "react";
+import Mensaje from "./Mensaje";
+
+const NuevoPresupuesto = ({ presupuesto, setPresupuesto }) => {
+  const [mensaje, setMensaje] = useState("");
+
+  const handlePresupuesto = (e) => {
+    e.preventDefault();
+
+    if (!Number(presupuesto) || Number(presupuesto) < 0) {
+      setMensaje("No es un presupuesto válido");
+    } else {
+      console.log("Si es un número");
+    }
+  };
+
+  return (
+    <div className="contenedor-presupuesto contenedor sombra">
+      <form onSubmit={handlePresupuesto} className="formulario">
+        <div className="campo">
+          <label htmlFor="presupuesto">Definir Presupuesto</label>
+          <input
+            id="presupuesto"
+            className="nuevo-presupuesto"
+            type="text"
+            placeholder="Añade tu Presupuesto"
+            value={presupuesto}
+            onChange={(e) => setPresupuesto(e.target.value)}
+          />
+        </div>
+        <input type="submit" value="Añadir" />
+
+        {mensaje && <Mensaje tipo="error">{mensaje}</Mensaje>}
+      </form>
+    </div>
+  );
+};
+
+export default NuevoPresupuesto;
